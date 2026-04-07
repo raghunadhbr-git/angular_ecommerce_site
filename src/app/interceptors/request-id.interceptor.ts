9f9de78b0bba73282b1d5bc0480fe75dabf30572
raghunadhbr-git <raghunadhbr@gmail.com>
2026-04-07 11:47:01 +0530

    Create request-id.interceptor.ts

---
+++ src/app/interceptors/request-id.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class RequestIdInterceptor implements HttpInterceptor {
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    // 🔥 Get existing OR create new
+    let requestId = localStorage.getItem('REQUEST_ID');
+
+    if (!requestId) {
+      requestId = 'REQ-' + Math.random().toString(36).substring(2, 10);
+      localStorage.setItem('REQUEST_ID', requestId);
+    }
+
+    // 🔥 Attach header
+    const modifiedReq = req.clone({
+      setHeaders: {
+        'X-Request-ID': requestId
+      }
+    });
+
+    return next.handle(modifiedReq);
+  }
+}
